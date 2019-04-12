f1236bb785559750b5bbaa62a1761391eda14914
Emmanuel Ostria <emmanuel.ostria@gruposhift-f6.com.mx>
2019-04-12 12:05:12 -0500

    Primer Commit

---
+++ src/mx/shf6/checksystem/MainApp.java
@@ -0,0 +1,16 @@
+package mx.shf6.checksystem;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
